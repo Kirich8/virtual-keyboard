7fff2995a4112ee965780721c5a7d9614f956228
Maxim Kirichenko <kirichenkomaxim@icloud.com>
2023-04-27 00:31:27 +0300

    feat: add keystroke listener

--- src/modules/keyboard/keyboard.js
+++ src/modules/keyboard/keyboard.js
@@ -4,14 +4,18 @@ import keysArray from '../keysInfo.js';
 export default class Keyboard {
   constructor() {
     this.keyboard = null;
+    this.output = null;
+    this.document = document;
   }
 
   render(block) {
+    this.output = document.querySelector('.board');
     this.keyboard = document.createElement('div');
     this.keyboard.className = 'keyboard';
     block.append(this.keyboard);
 
     this.renderKeys();
+    this.listenEvents();
   }
 
   renderKeys() {
@@ -20,6 +24,47 @@ export default class Keyboard {
       const { char, code, className } = keyInfo;
 
       key.render(this.keyboard, char, className, code);
+    });
+  }
+
+  insertText(char) {
+    this.output.setRangeText(char, this.output.selectionStart, this.output.selectionEnd, 'end');
+    this.output.focus();
+  }
+
+  addClass(code, className = 'click-button-green') {
+    this.document.querySelector(`[data-code="${code}"]`).classList.add(className);
+  }
+
+  removeClass(code, ...className) {
+    this.document.querySelector(`[data-code="${code}"]`).classList.remove(...className);
+  }
+
+  listenEvents() {
+    document.addEventListener('keydown', (event) => {
+      const codeButton = event.code;
+
+      keysArray.forEach((keyInfo) => {
+        if (codeButton === keyInfo.code) {
+          this.addClass(codeButton, 'click-button');
+          event.preventDefault();
+
+          this.insertText(keyInfo.char);
+        }
+      });
+    });
+
+    document.addEventListener('keyup', (event) => {
+      const classArray = ['click-button', 'click-button-green', 'click-button-space'];
+      const codeButton = event.code;
+
+      keysArray.forEach((keyInfo) => {
+        if (keyInfo.code === codeButton) {
+          setTimeout(() => {
+            this.removeClass(codeButton, ...classArray);
+          }, 50);
+        }
+      });
     });
   }
 }
